77c3c0bfa9601cd7edb915557bb4a45321af9459
Niels Lindgren <niels@uber.com>
2020-05-17 01:09:26 -0700

    Add ability to render the Behavior Tree

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module littlealbert
 
 go 1.14
 
-require github.com/stretchr/testify v1.5.1
+require (
+	github.com/go-redis/redis/v7 v7.2.0
+	github.com/sirupsen/logrus v1.6.0
+	github.com/stretchr/testify v1.5.1
+	github.com/xlab/treeprint v1.0.0
+)
---
+++ print.go
@@ -0,0 +1,55 @@
+package littlealbert
+
+import (
+	"fmt"
+
+	tp "github.com/xlab/treeprint"
+)
+
+// TreePrint renders the subtree as defined by the provided root
+// Node like you would see in the tree command.
+func TreePrint(root Node) string {
+	tree := tp.New()
+
+	p(root, tree)
+	return tree.String()
+}
+
+func p(node Node, tree tp.Tree) {
+	var label string
+	switch node.(type) {
+	case *conditional:
+		label = "Conditional"
+	case *task:
+		label = "Task"
+	case *sequence:
+		label = "Sequence"
+	case *fallback:
+		label = "Fallback"
+	case *parallel:
+		label = "Parallel"
+	case *decorator:
+		label = "Decorator"
+	case *dynamic:
+		label = "Dynamic"
+	default:
+		label = "Unknown Node"
+	}
+
+	if namer, ok := node.(NamedNode); ok && namer.Name() != "" {
+		label += fmt.Sprintf(": %s", namer.Name())
+	}
+
+	parent, ok := node.(ParentNode)
+	if ok {
+		branch := tree.AddBranch(label)
+
+		for _, child := range parent.Children() {
+			p(child, branch)
+		}
+
+		return
+	}
+
+	tree.AddNode(label)
+}
